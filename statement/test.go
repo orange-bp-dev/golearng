35aa7f98c52da532573ee09e82bf9993fe697477
Sato Ryoka <satoryoka0920@gmail.com>
2021-07-17 11:39:27 +0900

    answer check

--- statement/test.go
+++ statement/test.go
@@ -7,12 +7,30 @@ import (
 
 func main() {
 	//Q1. 以下のスライスから最大値・最小値を探して出力するコードを書いてください
+	//俺の回答
 	l := []int{100, 300, 23, 11, 23, 2, 4, 6, 4}
   sort.Sort(sort.IntSlice(l))
 	fmt.Println(l[0]) //最小値
 	fmt.Println(l[len(l)-1]) //最大値
 
+	//模範解答
+	var min int
+ 
+    for i, num := range l {
+        if i == 0 {
+            min = num
+            continue
+        }
+ 
+        if min >= num {
+            min = num
+        }
+    }
+ 
+    fmt.Println(min)
+
 	//Q2. 以下の果物の価格の合計を出力するコードを書いてください。
+	//俺の回答(模範解答と同じ！えらい！！)
 	m := map[string]int{
 		"apple":  200,
     "banana": 300,
